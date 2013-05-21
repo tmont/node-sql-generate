05f3edf62daee463e69a8a915875f1616b2236d2
tmont <tommy.mont@gmail.com>
2013-05-21 00:54:38 -0700

    set up travis probably

---
+++ scripts/create_test_users.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# create mysql user
+echo "create user root@localhost identified by 'password'; grant all on *.* to root@localhost identified by 'password';" \
+| mysql -u root -ppassword
+
+# create postgres user
+sudo su - postgres
+psql -d postgres -c "create user root with password 'password' superuser;"
+exit
