8f3be0281282813ecd5d10adb881f6c408e8ae65
tmont <tommy.mont@gmail.com>
2013-05-21 01:39:49 -0700

    run before_script in its own shell

--- scripts/create_test_users.sh
+++ scripts/create_test_users.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-# create mysql user
-echo "create user sqlgenerate@localhost identified by 'password'; grant all on *.* to sqlgenerate@localhost identified by 'password';" | mysql -u root
+(
+	# create mysql user
+	echo "create user sqlgenerate@localhost identified by 'password'; grant all on *.* to sqlgenerate@localhost identified by 'password';" | mysql -u root
 
-# create postgres user
-sudo su - postgres
-psql -d postgres -c "create user 'sqlgenerate' with password 'password' superuser;"
-exit
+	# create postgres user
+	sudo su - postgres
+	psql -d postgres -c "create user 'sqlgenerate' with password 'password' superuser;"
+)
