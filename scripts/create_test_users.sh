7b19961f082c0803f315f53af6459b9ec67d5fd6
tmont <tommy.mont@gmail.com>
2013-05-21 01:52:12 -0700

    apparently i don't know what cwd means

--- scripts/create_test_users.sh
+++ scripts/create_test_users.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # create mysql user
-mysql -u root -ppassword < ./create_mysql_user.sql
+mysql -u root -ppassword < ./scripts/create_mysql_user.sql
 
 # create postgres user
-sudo su -c "psql -d postgres -f ./create_postgres_user.sql" postgres
+sudo su -c "psql -d postgres -f ./scripts/create_postgres_user.sql" postgres
