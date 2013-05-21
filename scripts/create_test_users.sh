61ee287eb2dea0426881214b70044ba70146d1d7
tmont <tommy.mont@gmail.com>
2013-05-21 02:29:09 -0700

    frickin' frackin' firecrackin'

--- scripts/create_test_users.sh
+++ scripts/create_test_users.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # create mysql user
-mysql -u root -ppassword < ./scripts/create_mysql_user.sql
+mysql -u root < ./scripts/create_mysql_user.sql
 
 # create postgres user
 sudo su -c "psql -d postgres -f ./scripts/create_postgres_user.sql" postgres
