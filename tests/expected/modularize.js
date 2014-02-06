b127f011b3340d4a881476d71d767442c4debc77
tmont <tommy.mont@gmail.com>
2014-02-06 15:18:13 -0800

    added --modularize option

---
+++ tests/expected/modularize.js
@@ -0,0 +1,30 @@
+
+module.exports = function(sql) {
+
+
+	/**
+	 * SQL definition for node_sql_generate.bar
+	 */
+	exports.bar = sql.define({
+		name: 'bar',
+		columns: [
+			{ name: 'id' },
+			{ name: 'foo_id' }
+		]
+	});
+
+
+	/**
+	 * SQL definition for node_sql_generate.foo
+	 */
+	exports.foo = sql.define({
+		name: 'foo',
+		columns: [
+			{ name: 'id' },
+			{ name: 'field_1' },
+			{ name: 'foo_bar_baz' }
+		]
+	});
+
+
+};
