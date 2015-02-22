b63a8ee5ce277687921990d9e1aa34a79e60302d
tmont <tommy.mont@gmail.com>
2015-02-22 15:30:05 -0800

    don't need sudo, removed useless mysql command

--- provision.sh
+++ provision.sh
@@ -5,20 +5,17 @@ apt-get update
 apt-get install -y mysql-server postgresql libpq-dev
 
 # create mysql user
-mysql -u root
-
-# create mysql user
 mysql -u root < /vagrant/scripts/create_mysql_user.sql
 
 # create postgres user
-sudo su -c "psql -d postgres -f /vagrant/scripts/create_postgres_user.sql" postgres
+su -c "psql -d postgres -f /vagrant/scripts/create_postgres_user.sql" postgres
 
 # Allow remote connections to MySQL
-sudo sed -i 's/^bind-address/#bind-address/' /etc/mysql/my.cnf
-sudo restart mysql
+sed -i 's/^bind-address/#bind-address/' /etc/mysql/my.cnf
+restart mysql
 
 # Allow remote connections to Postgres
-sudo sed -i "s/^#listen_addresses = 'localhost'/listen_addresses = '\\*'/" /etc/postgresql/9.1/main/postgresql.conf
+sed -i "s/^#listen_addresses = 'localhost'/listen_addresses = '\\*'/" /etc/postgresql/9.1/main/postgresql.conf
 echo "host all all 0.0.0.0/0 md5" >> /etc/postgresql/9.1/main/pg_hba.conf
 
-sudo /etc/init.d/postgresql restart
+/etc/init.d/postgresql restart
